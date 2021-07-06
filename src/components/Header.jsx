bce842dd413027dd22feaaf8c12cbd09a089780c
Igor Denissov <igordenissov83@gmail.com>
2021-07-06 20:02:22 +0300

    added img; added styles for components;  added components: Header, Navbar, Profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+      <header className="header">
+        <img src="/images/test.jpg" alt="fail" />
+      </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+const Profile = () => {
+    return (
+      <div className ='content'>
+      <div>
+        <img src = "#" alt='fail'></img>
+      </div>
+      <div>
+        ava + description
+        <img src = "#" alt= 'fail'></img>
+      </div>
+      <div>
+        My posts
+          <div>
+          New post
+          </div>
+          <div>
+          <div>
+          post1
+          </div>
+          <div>
+          post2
+          </div>
+          </div>
+
+      </div>
+      Main content
+    </div>
+
+    );
+}
+
+export default Profile;
